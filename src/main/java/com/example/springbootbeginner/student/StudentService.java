048c645c55ba006ea93380b8a80aa60547a0b8f2
Jana Schaeffer <ja-schaeffer@web.de>
2024-01-16 15:04:41 +0100

    adding classes and controller

---
+++ src/main/java/com/example/springbootbeginner/student/StudentService.java
@@ -0,0 +1,4 @@
+package com.example.springbootbeginner.student;
+
+public class StudentService {
+}
---
+++ src/test/java/com/example/springbootbeginner/SpringbootbeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
