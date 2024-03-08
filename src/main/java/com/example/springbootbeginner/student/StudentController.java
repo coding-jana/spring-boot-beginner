0d3e4be09be659d672fac1de0c0c0bf1fafa95ea
Jana Schaeffer <ja-schaeffer@web.de>
2024-03-08 18:01:27 +0100

    adding PutMapping

--- src/main/java/com/example/springbootbeginner/student/StudentController.java
+++ src/main/java/com/example/springbootbeginner/student/StudentController.java
@@ -23,5 +23,16 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     }
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @PathVariable(required = false) String name,
+                              @PathVariable(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
+    }
 
 }
