d80e46e71a541f775be71fabcd55e68cee7f2cdb
Jana Schaeffer <ja-schaeffer@web.de>
2024-03-08 16:52:41 +0100

    adding service, repository and config

---
+++ src/main/java/com/example/springbootbeginner/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.springbootbeginner.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student alexa = new Student(
+                    "Alexa",
+                    "alexa@gmail.com",
+                    24,
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    24,
+                    LocalDate.of(2000, Month.JANUARY, 6)
+            );
+
+            repository.saveAll(
+                    List.of(alexa, alex)
+            );
+        };
+    }
+}
